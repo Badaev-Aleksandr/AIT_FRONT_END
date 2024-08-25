87d7a7e6c65e8a313e3014ca5fed8b1a89ffdac1
Aleksandr <badaev92aleksandr@gmail.com>
2024-08-25 14:16:41 +0200

    Code formating

--- homeworks/homework_09/task_05.js
+++ homeworks/homework_09/task_05.js
@@ -1,7 +1,7 @@
 /*Объявить массив из 5 элементов и заполнить его любыми числами. 
 Присвоить 5 переменным значения из массива с помощью деструктуризации.*/
-let arr = [1,2,3,4,5];
-let[one, two, three, four, five] = arr;
+let arr = [1, 2, 3, 4, 5];
+let [one, two, three, four, five] = arr;
 console.log(one);
 console.log(two);
 console.log(three);
