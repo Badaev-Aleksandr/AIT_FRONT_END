09bcc76a30c2e0c5c69f88d5c87eb42e235058bc
Aleksandr <badaev92aleksandr@gmail.com>
2024-09-03 20:14:08 +0200

    lesson 14 HW

--- homeworks/homework_13/index.js
+++ homeworks/homework_13/index.js
@@ -3,10 +3,9 @@ let firstListItem = document.createElement("li");
 let lastListItem = document.createElement("li");
 firstListItem.textContent = "Firs";
 lastListItem.textContent = "Last";
-let first = document.querySelector("ol");
-first.prepend(firstListItem);
-let last = document.querySelector("ol");
-last.append(lastListItem);
+let olElement = document.querySelector("ol");
+olElement.prepend(firstListItem);
+olElement.append(lastListItem);
 
 //2. Изменить текст второго элемента списка
 let twoElementList = document.querySelector(".list li:nth-child(2)");
@@ -15,9 +14,6 @@ twoElementList.textContent = "I am a changed element";
 //3. Удалить третий элемент
 let deletedElement = document.querySelector(".list li:nth-child(3)");
 deletedElement.remove();
-deletedElement.textContent = "I'm a removed element";
-let deletedListItem = document.querySelector("ol");
-deletedListItem.after(deletedElement);
 
 //4. Изменить стили по своему усмотрению для четвертого элемента
 let changeStyle = document.querySelector(".list li:nth-child(4)");
