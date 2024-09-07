809a4c298f8a0c5ccb74c40a70e7f659fb388a5e
Aleksandr <badaev92aleksandr@gmail.com>
2024-09-08 01:57:59 +0200

    update

--- lessons/lesson_15/project/index.js
+++ lessons/lesson_15/project/index.js
@@ -1,18 +1,10 @@
 let buttonResult = document.querySelector("#cread-button");
-console.log(buttonResult);
 let form = document.querySelector("#form");
-console.log(form);
 let cardContainer = document.querySelector(".card-container");
-console.log(cardContainer);
-
 let resultName = document.querySelector(".result-name");
-console.log(resultName);
 let resultSurname = document.querySelector(".result-surname");
-console.log(resultSurname);
 let resultAge = document.querySelector(".result-age");
-console.log(resultAge);
 let resultJob = document.querySelector(".result-job");
-console.log(resultJob);
 
 let addCardFunction = () => {
   cardContainer.classList.add("visible");
@@ -24,7 +16,9 @@ let hideCardFunction = () => {
 
 let getDate = (event) => {
   let check = true;
+
   event.preventDefault();
+  
   let userNameValue = form.userName.value;
   resultName.textContent = userNameValue;
   let userSurnameValue = form.userSurname.value;
