04b62a985aac180f27c03fae2da5bd4f83fb1852
Aleksandr <badaev92aleksandr@gmail.com>
2024-09-28 02:56:44 +0200

    project update

--- lessons/lesson_20/lesson project/index.js
+++ lessons/lesson_20/lesson project/index.js
@@ -25,34 +25,61 @@ http://openweathermap.org/img/w/${icon}.png
 
 const getWeatherButton = document.querySelector("#get-weather-button");
 const getCityInput = document.querySelector("#city");
-// const resultWeather = document.querySelector(".result-weather-container");
 const resultTemp = document.querySelector(".result-temp");
 const resultImgWeather = document.querySelector(".result-icon");
 const weatherForm = document.querySelector("#weather-form");
 const userAppId = "2669988ce3f2970e0d287742951e368a";
+const spinner = document.querySelector(".spinner");
+
+const showSpinner = () => {
+  spinner.style.display = "block";
+};
+
+const hideSpinner = () => {
+  spinner.style.display = "none";
+};
 
 const getWeather = async () => {
-  const getCity = getCityInput.value.trim();
+  try {
+    resultTemp.innerHTML = ``;
+    resultImgWeather.innerHTML = ``;
+    showSpinner();
+    const getCity = getCityInput.value.trim();
 
-  let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${getCity}&appid=${userAppId}`;
+    if (getCity.length !== 0) {
+      let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${getCity}&appid=${userAppId}`;
 
-  const response = await fetch(weatherUrl);
-  const weather = await response.json();
-  console.log(weather);
-  if (response.ok) {
-    let temp = weather.main.temp;
-    temp = (temp - 273.15).toFixed(0);
+      const response = await fetch(weatherUrl);
+      const weather = await response.json();
+      console.log(weather);
 
-    const icon = weather.weather[0].icon;
-    const iconUrl = `http://openweathermap.org/img/w/${icon}.png`;
+      if (response.ok) {
+        resultTemp.classList.remove("error")
+        let temp = weather.main.temp;
+        temp = (temp - 273.15).toFixed(0);
 
-    resultTemp.textContent = `${getCity}: ${temp}°C`;
-    resultImgWeather.innerHTML = `
+        const icon = weather.weather[0].icon;
+        const iconUrl = `http://openweathermap.org/img/w/${icon}.png`;
+
+        resultTemp.textContent = `${getCity}: ${temp}°C`;
+        resultImgWeather.innerHTML = `
     <img src ="${iconUrl}">
     `;
+      } else {
+        throw Object.assign(new Error("Some Error"), {
+          responseError: weather,
+        });
+      }
+    } else {
+      alert("Please enter City!");
+    }
+  } catch (error) {
+    resultTemp.classList.add("error");
+    resultTemp.innerHTML = `Error status: ${error.responseError.cod}<br>Error message: ${error.responseError.message}`;
+  } finally {
+    hideSpinner();
+    weatherForm.reset();
   }
-
-  weatherForm.reset();
 };
 
 getWeatherButton.addEventListener("click", getWeather);
